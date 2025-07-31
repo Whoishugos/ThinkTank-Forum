37869d15a0739a39f547ebd9ac37a89b81f5f211
whoishugo <tommyonair22@gmail.com>
2025-07-31 13:17:29 +0700

    add UI Home : Helps Others, CTA, Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,6 +81,121 @@
         </div>
     </div>
 </section>
+<section class="bg-gray">
+    <div class="container py-80px">
+    <h2 class="text-center mb-5">Help Others</h2>
+    <div class="row">
+        <div class="col-12 col-lg-4 mb-3">
+            <div class="card">
+                <a href="/">
+                <h3>How to add a custom validation in laravel?</h3>
+                </a>
+                <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user roles, among which, the ...</p>
+                <div class="row">
+                    <div class="col me-1 me-lg-2">
+                        <a href="/"><span class="badge rounded-pill text-bg-light">Eloquent</span></a>
+                    </div>
+                    <div class="col-5 col-lg-7">
+                        <div class="avatar-sm-wrapper d-inline-block">
+                            <a href="" class="me-1">
+                                <img src="{{ asset('images/profile.png') }}" class="avatar rounded-circle">
+                            </a>
+                        </div>
+                        <span class="fs-12px">
+                            <a href="/" class="me-1 fw-bold">Hugo</a>
+                            <span class="bolor-gray">5 Minutes</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 mb-3">
+            <div class="card">
+                <a href="/">
+                <h3>How to add a custom validation in laravel?</h3>
+                </a>
+                <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user roles, among which, the ...</p>
+                <div class="row">
+                    <div class="col me-1 me-lg-2">
+                        <a href="/"><span class="badge rounded-pill text-bg-light">Eloquent</span></a>
+                    </div>
+                    <div class="col-5 col-lg-7">
+                        <div class="avatar-sm-wrapper d-inline-block">
+                            <a href="" class="me-1">
+                                <img src="{{ asset('images/profile.png') }}" class="avatar rounded-circle">
+                            </a>
+                        </div>
+                        <span class="fs-12px">
+                            <a href="/" class="me-1 fw-bold">Hugo</a>
+                            <span class="bolor-gray">5 Minutes</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 mb-3">
+            <div class="card">
+                <a href="/">
+                <h3>How to add a custom validation in laravel?</h3>
+                </a>
+                <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user roles, among which, the ...</p>
+                <div class="row">
+                    <div class="col me-1 me-lg-2">
+                        <a href="/"><span class="badge rounded-pill text-bg-light">Eloquent</span></a>
+                    </div>
+                    <div class="col-5 col-lg-7">
+                        <div class="avatar-sm-wrapper d-inline-block">
+                            <a href="" class="me-1">
+                                <img src="{{ asset('images/profile.png') }}" class="avatar rounded-circle">
+                            </a>
+                        </div>
+                        <span class="fs-12px">
+                            <a href="/" class="me-1 fw-bold">Hugo</a>
+                            <span class="bolor-gray">5 Minutes</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<section class="container min-h-372px d-flex flex-column align-items-center justify-content-center">
+<h2>Ready to contribute?</h2>
+<p class="mb-4">Want to make a big impact?</p>
+<div class="text-center">
+    <a class="btn btn-primary me-2 mb-2 mb-lg-0" href="">Sign Up</a>
+    <a class="btn btn-secondary mb-2 mb-lg-0" href="">Join the Discussion</a>
+</div>
+</section>
+<footer class="py-80px">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6 mb-5 mb-lg-0">
+                <img src="{{ asset('images/thinktank-red.png') }}" alt="think-tank" class="h-32px mb-4">
+                <p class="text-white">ruang terbuka bagi para pemikir kritis, inovator, dan <br/> pembelajar untuk berdiskusi, berbagi ide, dan menciptakan solusi nyata.</p>
+            </div>
+            <div class="col-12 col-lg-6 me-auto">
+                <div class="d-flex flex-column flex-lg-row justify-content-end">
+                    <div class="d-flex flex-column me-140px mb-3 mb-lg-0">
+                        <p class="fw-bold fs-5 text-white text-nowrap">Contact Us</p>
+                        <ul class="list-unstyled">
+                            <li class="text-white"><a href="mailto:info@thinktank.id">info@thinktank.id</a></li>
+                        </ul>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <p class="fw-bold text-white fs-5">Links</p>
+                        <ul class="list-unstyled">
+                            <li class="text-white mb-2"><a href="#">Home</a></li>
+                            <li class="text-white mb-2"><a href="#">Discussions</a></li>
+                            <li class="text-white mb-2"><a href="#">About Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
